e49e6d8ab5709f42fbeefa16cc94437cc7baa736
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-27 11:15:27 +0100

    added valisation to form

--- controllers/note-create.controller.php
+++ controllers/note-create.controller.php
@@ -3,11 +3,28 @@ $heading = "create";
 $config = require "config.php";
 $db = new Database($config['database'] );
 
+$err = [];
+
 if ($_SERVER['REQUEST_METHOD']==='POST'){
+   
     $body = $_POST['body'];
     $id = 3;
-    $notes = $db->query('INSERT INTO notes (notes, user_id) VALUES (?, ?)',[
-       $body,$id
-    ]);  
+
+    if (strlen($body) === 0){
+        $err['body'] = "A body is required";
+    }
+
+    if (strlen($body) > 1000){
+        $err['body'] = "The body cannot be more than 1000 characters";
+    }
+
+
+    if (empty(($err))){
+
+        $notes = $db->query('INSERT INTO notes (notes, user_id) VALUES (?, ?)',[
+            $body,$id
+         ]); 
+    }
+     
 }
 require "views/note-create.view.php";
