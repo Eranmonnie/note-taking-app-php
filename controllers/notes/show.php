26ce9541550de00a83ff147c967d87b62d40a261
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-30 17:53:04 +0100

    delete request

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,6 +5,29 @@ use core\Database;
 $heading ="Note";
 $config = require base_path("config.php");
 $db = new Database($config['database'] );
+$user_id = 3;
+
+if ($_SERVER['REQUEST_METHOD'] ==='POST'){
+    
+    $id = $_POST['id'];
+    $query = 'select * from notes where id = ?';
+
+    //handels failed query  "what if user accesses data that dosent exis"
+    $note = $db->query($query, [$id])->fetchorFail();
+
+    authorize($note['user_id'] === $user_id);
+    //if autentic user is acessing his data and wants to delete
+
+    $query = 'delete from notes where id = ?';
+
+    //handels failed query  "what if user accesses data that dosent exis"
+    $db->query($query, [$id]);
+
+    //after deleting data
+    header('location: /notes');
+    exit();
+   
+}
 
 $id = $_GET['id'];
 $query = 'select * from notes where id = ?';
@@ -12,10 +35,11 @@ $query = 'select * from notes where id = ?';
 $note = $db->query($query, [$id])->fetchorFail();
 
 //we havent reviewed sessions but if we were to secure a page from autenticate users we would have to match the logged in users id with ehr user_id foreign key in the notes table leta=s say the logged in user has an id of 3
-authorize($note['user_id'] === 3);
+authorize($note['user_id'] === $user_id);
 
 
 view("notes/show.view.php", [
     "heading" => $heading,
     "note"=> $note,
 ]);//loads views  
+
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -4,6 +4,12 @@
 
  <h1>NOTE</h1>   
  <p><?= htmlspecialchars($note['notes'])?></p>  
+
+ <!-- Delete -->
+ <form action="/note" method="POST">
+    <input type="hidden" name = "id" value="<?=$note['id']?>">
+    <button>Delete</button>
+ </form>
  
  <div><a href="/notes/note-create">Create</a></div>
  
