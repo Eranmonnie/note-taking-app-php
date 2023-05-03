e53ea30a7f96646f99e295cf113688be9be99321
Eranmonnie <ajalaferanmi123@gmail.com>
2023-05-03 17:39:03 +0100

    auth for signup for duplicate email in db

--- controllers/auth/store.php
+++ controllers/auth/store.php
@@ -23,6 +23,18 @@ if(! Validate::string($password, 7, 255)){
     $err['password'] = "invalid password";
 }
 
+ //query db for useer if email exists 
+$check = $db->query("select * from users where email = ? ",[
+    $email,
+])->fetch();
+
+//check if email exists
+if ($check){
+
+    $err['email'] = "email already exists";
+
+}
+
 if (! empty(($err))){
     return view("auth/create.view.php", [
         "heading" => $heading,
@@ -34,10 +46,15 @@ if (! empty(($err))){
 $User = $db->query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [
 $name,$email,$password]);
 
+$_SESSION['user'] =[
+    'email'=> $email
+];
+
 header('location: /');
+exit();
 
 
-//chaeck if user esists
+
 
     //yes ? log he user in and set session 
 
